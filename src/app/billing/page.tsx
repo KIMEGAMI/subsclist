d217824f6b3beb93d4411119e96442ed20229a40
KIMEGAMI <workshinji2023@gmail.com>
2026-07-17 11:08:25 +0900

    Remove PR #4 billing page

--- src/app/billing/page.tsx
+++
@@ -1,8 +0,0 @@
-import { BillingView } from "@/components/admin-views";
-import { noIndexMetadata } from "@/lib/seo";
-
-export const metadata = noIndexMetadata;
-
-export default function BillingPage() {
-  return <BillingView />;
-}
