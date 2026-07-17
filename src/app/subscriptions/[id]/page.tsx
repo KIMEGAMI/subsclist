4fd27cdfd5d313524a40ea2bad9e3ceaf7584b4b
KIMEGAMI <workshinji2023@gmail.com>
2026-07-17 11:01:36 +0900

    Revert PR #4 subscription detail page changes

--- src/app/subscriptions/[id]/page.tsx
+++ src/app/subscriptions/[id]/page.tsx
@@ -1,7 +1,4 @@
-import { SubscriptionDetailView } from "@/components/subscription-views";
-import { noIndexMetadata } from "@/lib/seo";
-
-export const metadata = noIndexMetadata;
+import { SubscriptionDetailView } from "@/components/real-views";
 
 export default async function SubscriptionDetailPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
