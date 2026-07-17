362a945951ed9819702999e2c348ad34226d2a83
KIMEGAMI <workshinji2023@gmail.com>
2026-07-17 11:09:30 +0900

    Remove PR #4 maintenance page

--- src/app/maintenance/page.tsx
+++
@@ -1,8 +0,0 @@
-import { MaintenanceView } from "@/components/public-views";
-import { noIndexMetadata } from "@/lib/seo";
-
-export const metadata = noIndexMetadata;
-
-export default function MaintenancePage() {
-  return <MaintenanceView />;
-}
